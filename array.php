f709c05a50047eb18c15346ff6c044445d09a7ae
sskyaa <saskiatripani@gmail.com>
2025-11-04 20:59:13 +0700

    Menambahkan Setup Awal

---
+++ array.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h2> === POLGAN MART === </h2>";
+
+$nama_barang   = ["Tas Ransel", "Sepatu", "Seragam", "Hijab Segi Empat", "Dasi"];
+$harga_barang  = [120000, 200000, 400000, 30000, 10000];
