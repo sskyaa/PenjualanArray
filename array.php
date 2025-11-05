afc71c67b231d6de066b07bb6e4bdfe71388e3cf
sskyaa <saskiatripani@gmail.com>
2025-11-05 14:26:19 +0700

    Menambahkan Kode Barang

--- array.php
+++ array.php
@@ -1,9 +1,10 @@
 <?php
 echo "<h2 style='text-align:center; color:#5a4633;'>🧾  POLGAN MART  🧾</h2>";
-echo "<hr style='width:45%; border:1px solid #8b6b4f;'>";
+echo "<hr style='width:55%; border:1px solid #8b6b4f;'>";
 
 $nama_barang   = ["Tas Ransel", "Sepatu", "Seragam", "Hijab Segi Empat", "Dasi"];
 $harga_barang  = [120000, 200000, 400000, 30000, 10000];
+$kode_barang   = ["AR01", "AR02", "AR03", "AR04", "AR05"];
 
 shuffle($nama_barang);
 
@@ -22,18 +23,20 @@ for ($i = 0; $i < $jumlah_produk; $i++) {
 }
 
 echo "<div style='display:flex; justify-content:center; padding:30px;'>";
-echo "<div style='border:3px solid #8b6b4f; border-radius:12px; padding:20px; width:575px; background-color:#fdf6ec; box-shadow:4px 4px 10px rgba(139,107,79,0.3);'>";
+echo "<div style='border:3px solid #8b6b4f; border-radius:12px; padding:30px; width:648px; background-color:#fdf6ec; box-shadow:4px 4px 10px rgba(139,107,79,0.3);'>";
 echo "<pre style='font-family:Courier New; font-size:16px; text-align:left; color:#4b3a2f; margin:0;'>";
 
-echo "No  Nama Barang            Qty    Harga         Total";
-echo "\n--------------------------------------------------------\n";
-
+$header = "No  Kode   Nama Barang            Qty   Harga           Total";
+echo $header . "\n";
+echo str_repeat("-", strlen($header)) . "\n";
 
 $no = 1;
 foreach ($beli as $i => $barang) {
     $harga = $total[$i] / $jumlah[$i];
-    printf("%-3s %-22s %-6s Rp %-10s Rp %-10s\n",
+    $kode = $kode_barang[array_search($barang, ["Tas Ransel", "Sepatu", "Seragam", "Hijab Segi Empat", "Dasi"])];
+    printf("%-3s %-6s %-22s %-5s Rp %-12s Rp %-12s\n",
         $no,
+        $kode,
         $barang,
         $jumlah[$i],
         number_format($harga, 0, ',', '.'),
@@ -44,9 +47,9 @@ foreach ($beli as $i => $barang) {
 }
 
 
-echo "--------------------------------------------------------\n";
-printf("%-42s Rp %-12s\n", "Total Penjualan :", number_format($grandtotal, 0, ',', '.'));
-echo "--------------------------------------------------------\n";
+echo str_repeat("-", strlen($header)) . "\n";
+printf("%-55s Rp %-12s\n", "Total Belanja :", number_format($grandtotal, 0, ',', '.'));
+echo str_repeat("-", strlen($header)) . "\n";
 echo "Terima kasih telah berbelanja di POLGAN MART 😊";
 
 echo "</pre>";
